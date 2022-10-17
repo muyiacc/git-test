28f1a35d65abc8adb48992831e46028fe60f9293
muyiacc <muyiacc@163.com>
2022-10-17 11:22:06 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author muyiacc
+ * @version v1.0
+ * @Description TODO
+ * @Date 2022-10-17 11:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git~");
+    }
+}
